a582295b93f87d15036f5ded85f6ae975d2b6194
WenziLike <SlavaMyraxin@icloud.com>
2020-06-20 02:13:19 +0200

    checkFor Empty String

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 // "use strict";
 //===================================================================
-// ! сделать вывод в выпадающий список стран мира 
-//! сделать настоящее время
-// ! вывести погоду
+//! сделать проверку инпут
+//! сделать скрытие селекта  города и какой селект выбран
 
 //===================================================================
 
 const Weather = function () {
-    const city = document.querySelector('.weather__city');
     const countriesSelected = document.querySelector('.dropdown__countries');
     const citiesSelected = document.querySelector('.dropdown__cities');
+    const city = document.querySelector('.weather__city');
 
     //===============================================================
     //! получил JSON file
@@ -27,41 +26,55 @@ const Weather = function () {
         }
         //=================
         // ! добавляем страны и города
-        let countries = Object.keys(data);
+        const countries = Object.keys(data);
         countries.sort();
         addOptions(countriesSelected, countries);
 
-        let cities = data[countries[0]];
+
+        const cities = data[countries[0]];
         addOptions(citiesSelected, cities);
         //=================
-
         countriesSelected.addEventListener('change', function () {
-            let listCities = data[this.value];
+            const listCities = data[this.value];
             listCities.sort();
             citiesSelected.length = 0;
 
-            addOptions(citiesSelected, listCities);
-        });
-        //===============================================================
+            // let index = countriesSelected.selectedIndex;
+            // console.log('selectedIndex: ' + index);// выбранный селект
 
-        // !  рализация Enter вместо  Button.
-        searchCity.addEventListener('keydown', function (enterClick) {
-            if (enterClick.keyCode === 13) {
-                city.textContent = this.value;
-                searchCity.value = '';
-            }
+
+
+
+
+            addOptions(citiesSelected, listCities);
         });
     })();
     //===============================================================
+    // !  рализация Enter вместо  Button.
+
     const icon = document.querySelector('.icon-search');
     const searchLocat = document.querySelector('.search__location');
     const searchCity = document.querySelector('.search__city');
+
 
     icon.addEventListener('click', () => {
         searchCity.classList.toggle("active");
         searchLocat.classList.toggle("active");
         document.querySelector("input[type='text']").focus();
 
+    });
+
+    searchCity.addEventListener('keydown', function (enterClick) {
+
+        let checkFoEmpStr = '';
+        checkFoEmpStr = checkFoEmpStr.replace(/^\s+|\s+$/g, '');
+
+        if (enterClick.key === "Enter" && searchCity.value == checkFoEmpStr) {
+            alert('noooo');
+        } else if (enterClick.key === "Enter") {
+            city.textContent = this.value;
+            searchCity.value = '';
+        }
     });
 
     //===============================================================
